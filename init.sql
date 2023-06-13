872f55cfc189ffc8e729b1fc7b2b276fe580a3c0
Samuel Escobar <samueb8@gmail.com>
2023-06-13 08:36:11 -0400

    correccion final

--- init.sql
+++ init.sql
@@ -297,21 +297,38 @@ BEGIN
   END IF;
 END //
 
+CREATE TRIGGER tr_verificar_salario_insert
+BEFORE INSERT ON empleado
+FOR EACH ROW
+BEGIN
+  IF NEW.salario < 2362 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El salario debe ser superior a 2362';
+  END IF;
+END //
+
+CREATE TRIGGER tr_verificar_salario_update
+BEFORE UPDATE ON empleado
+FOR EACH ROW
+BEGIN
+  IF NEW.salario < 2362 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El salario debe ser superior a 2362';
+  END IF;
+END //
+
+CREATE TRIGGER tr_fecha_fin_superior_fecha_inicio
+BEFORE INSERT ON empleado
+FOR EACH ROW
+BEGIN
+  IF NEW.fecha_fin <= NEW.fecha_inicio THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha_fin debe ser superior a la fecha_inicio';
+  END IF;
+END//
+
 DELIMITER ;
 
 
 -- Stored Procedures
 DELIMITER //
-
-CREATE PROCEDURE sp_insertar_registro_comercial(
-  IN p_empresa_id INT,
-  IN p_fecha_registro DATE,
-  IN p_fecha_vencimiento DATE
-)
-BEGIN
-  INSERT INTO registro_comercial (empresa_id, fecha_registro, fecha_vencimiento)
-  VALUES (p_empresa_id, p_fecha_registro, p_fecha_vencimiento);
-END //
 
 CREATE PROCEDURE sp_modificar_empleado(
   IN p_persona_ci INT,
@@ -325,17 +342,6 @@ BEGIN
   UPDATE empleado
   SET cargo = p_cargo, salario = p_salario, fecha_inicio = p_fecha_inicio, fecha_fin = p_fecha_fin
   WHERE persona_ci = p_persona_ci AND empresa_id = p_empresa_id;
-END //
-
-CREATE PROCEDURE sp_insertar_direccion(
-  IN p_numero INT,
-  IN p_calle VARCHAR(100),
-  IN p_barrio VARCHAR(100),
-  IN p_departamento_id INT
-)
-BEGIN
-  INSERT INTO direccion (numero, calle, barrio, departamento_id)
-  VALUES (p_numero, p_calle, p_barrio, p_departamento_id);
 END //
 
 -- Verificar que la foreign key de persona_ci exista en la tabla persona en el stored procedure
@@ -383,13 +389,6 @@ BEGIN
   VALUES (p_empresa_id, p_direccion_id);
 END //
 
-DELIMITER ;
-
--- Agregar 5 stored procedures relevantes
-
--- Stored Procedure para calcular el salario promedio de los empleados de una empresa
-DELIMITER //
-
 CREATE PROCEDURE sp_calcular_salario_promedio(
   IN p_empresa_id INT,
   OUT p_salario_promedio DECIMAL(10, 2)
@@ -400,36 +399,33 @@ BEGIN
   WHERE empresa_id = p_empresa_id;
 END //
 
-DELIMITER ;
-
 -- Stored Procedure para obtener la lista de personas mayores de edad
-DELIMITER //
 
-CREATE PROCEDURE sp_obtener_personas_mayores_edad()
+CREATE PROCEDURE sp_obtener_empleados_mayores_edad(
+  IN p_edad INT
+)
 BEGIN
   SELECT *
   FROM persona
-  WHERE DATEDIFF(CURDATE(), fecha_nacimiento) >= 18;
+  WHERE DATEDIFF(CURDATE(), fecha_nacimiento) >= p_edad;
 END //
 
-DELIMITER ;
 
--- Stored Procedure para eliminar un registro de empleado
-DELIMITER //
+-- Stored Procedure para quitar la disponibilidad de un registro de empleado
 
-CREATE PROCEDURE sp_eliminar_empleado(
+CREATE PROCEDURE sp_modificar_estado_empleado(
   IN p_persona_ci INT,
-  IN p_empresa_id INT
+  IN p_empresa_id INT,
+  IN p_disponible BOOLEAN
 )
 BEGIN
-  DELETE FROM empleado
+  UPDATE empleado
+  SET disponible = p_disponible
   WHERE persona_ci = p_persona_ci AND empresa_id = p_empresa_id;
 END //
 
-DELIMITER ;
 
 -- Stored Procedure para actualizar el capital de una empresa
-DELIMITER //
 
 CREATE PROCEDURE sp_actualizar_capital_empresa(
   IN p_empresa_id INT,
@@ -441,16 +437,199 @@ BEGIN
   WHERE id = p_empresa_id;
 END //
 
-DELIMITER ;
-
--- Stored Procedure para obtener las direcciones disponibles
-DELIMITER //
-
-CREATE PROCEDURE sp_obtener_direcciones_disponibles()
+CREATE PROCEDURE sp_buscar_empresas_por_capital(
+  IN p_monto_capital DECIMAL(10, 2)
+)
 BEGIN
   SELECT *
-  FROM direccion
-  WHERE disponible = true;
+  FROM empresa
+  WHERE capital > p_monto_capital;
 END //
 
+CREATE PROCEDURE sp_buscar_empresas_por_porcentaje_participacion(
+  IN p_porcentaje DECIMAL(5, 2)
+)
+BEGIN
+  SELECT *
+  FROM empresa e
+  INNER JOIN socio s ON e.id = s.empresa_id
+  WHERE s.porcentaje_participacion > p_porcentaje;
+END //
+
+CREATE PROCEDURE sp_buscar_empresas_por_cantidad_acciones(
+  IN p_cantidad_acciones INT
+)
+BEGIN
+  SELECT *
+  FROM empresa e
+  INNER JOIN accionista a ON e.id = a.empresa_id
+  WHERE a.cantidad_acciones > p_cantidad_acciones;
+END //
+
+CREATE PROCEDURE sp_buscar_personas_menores_salario(
+  IN p_edad INT,
+  IN p_salario DECIMAL(10, 2)
+)
+BEGIN
+  SELECT p.*
+  FROM persona p
+  INNER JOIN empleado e ON p.ci = e.persona_ci
+  WHERE DATEDIFF(CURDATE(), p.fecha_nacimiento) < p_edad
+    AND e.salario > p_salario;
+END //
+
+
+
 DELIMITER ;
+
+-- Indices
+CREATE INDEX idx_empresa_capital ON empresa (capital);
+CREATE INDEX idx_socio_porcentaje_participacion ON socio (porcentaje_participacion);
+CREATE INDEX idx_accionista_cantidad_acciones ON accionista (cantidad_acciones);
+CREATE INDEX idx_empleado_salario ON empleado (salario);
+CREATE INDEX idx_persona_fecha_nacimiento ON persona (fecha_nacimiento);
+
+
+-- Tabla: departamento
+INSERT INTO departamento (id, nombre)
+VALUES (1, 'La Paz'),
+       (2, 'Santa Cruz'),
+       (3, 'Cochabamba'),
+       (4, 'Beni'),
+       (5, 'Chuquisaca'),
+       (6, 'Pando'),
+       (7, 'Oruro'),
+       (8, 'Potosi'),
+       (9, 'Tarija');
+
+-- Tabla: direccion
+INSERT INTO direccion (numero, calle, barrio, departamento_id)
+VALUES (123, 'Calle Principal', 'Centro', 1),
+       (456, 'Avenida del Sol', 'Residencial', 2),
+       (789, 'Calle de la Luna', 'Oeste', 3),
+       (234, 'Avenida de los Árboles', 'Este', 4),
+       (567, 'Calle del Mar', 'Playa', 5),
+       (890, 'Avenida de las Flores', 'Jardín', 6),
+       (345, 'Calle de los Pinos', 'Bosque', 7),
+       (678, 'Avenida de las Montañas', 'Montaña', 8),
+       (901, 'Calle de las Estrellas', 'Estrellado', 9),
+       (123, 'Avenida de los Ríos', 'Riberas', 2);
+
+-- Tabla: persona
+INSERT INTO persona (ci, nombre, apellido_paterno, apellido_materno, fecha_nacimiento)
+VALUES (123456, 'Juan', 'Pérez', 'Gómez', '1990-01-01'),
+       (234567, 'María', 'González', 'López', '1991-02-02'),
+       (345678, 'Carlos', 'Ramírez', 'Hernández', '1992-03-03'),
+       (456789, 'Ana', 'Torres', 'Sánchez', '1993-04-04'),
+       (567890, 'Pedro', 'Martínez', 'Rodríguez', '1994-05-05'),
+       (678901, 'Laura', 'Díaz', 'Fernández', '1995-06-06'),
+       (789012, 'Luis', 'Vargas', 'Jiménez', '1996-07-07'),
+       (890123, 'Marta', 'Moreno', 'García', '1997-08-08'),
+       (901234, 'José', 'López', 'Silva', '1998-09-09'),
+       (224341, 'Sofía', 'Herrera', 'Ortega', '1999-10-10');
+
+-- Tabla: empresa
+INSERT INTO empresa (nombre, direccion_id, capital, rubro)
+VALUES ('Apple', 1, 1000000.00, 'Tecnología'),
+       ('Samsung', 2, 2000000.00, 'Electrónica'),
+       ('Microsoft', 3, 3000000.00, 'Software'),
+       ('Amazon', 4, 4000000.00, 'Comercio Electrónico'),
+       ('Google', 5, 5000000.00, 'Internet'),
+       ('Facebook', 6, 6000000.00, 'Redes Sociales'),
+       ('Tesla', 7, 7000000.00, 'Automotriz'),
+       ('Netflix', 8, 8000000.00, 'Entretenimiento'),
+       ('Nike', 9, 9000000.00, 'Deportes'),
+       ('Coca-Cola', 10, 10000000.00, 'Bebidas');
+
+-- Tabla: registro_comercial
+INSERT INTO registro_comercial (empresa_id, fecha_registro, fecha_vencimiento)
+VALUES (1, '2023-01-01', '2025-01-01'),
+       (2, '2023-02-01', '2025-02-01'),
+       (3, '2023-03-01', '2025-03-01'),
+       (4, '2023-04-01', '2025-04-01'),
+       (5, '2023-05-01', '2025-05-01'),
+       (6, '2023-06-01', '2025-06-01'),
+       (7, '2023-07-01', '2025-07-01'),
+       (8, '2023-08-01', '2025-08-01'),
+       (9, '2023-09-01', '2025-09-01'),
+       (10, '2023-10-01', '2025-10-01');
+
+-- Tabla: accionista
+INSERT INTO accionista (persona_ci, empresa_id, cantidad_acciones)
+VALUES (123456, 1, 100),
+       (234567, 2, 200),
+       (345678, 3, 300),
+       (456789, 4, 400),
+       (567890, 5, 500),
+       (678901, 6, 600),
+       (789012, 7, 700),
+       (890123, 8, 800),
+       (901234, 9, 900),
+       (123456, 10, 1000);
+
+-- Tabla: socio
+INSERT INTO socio (persona_ci, empresa_id, porcentaje_participacion)
+VALUES (123456, 1, 10.5),
+       (234567, 2, 20.5),
+       (345678, 3, 30.5),
+       (456789, 4, 40.5),
+       (567890, 5, 50.5),
+       (678901, 6, 60.5),
+       (789012, 7, 70.5),
+       (890123, 8, 80.5),
+       (901234, 9, 90.5),
+       (123456, 10, 100.0);
+
+-- Tabla: representante_legal
+INSERT INTO representante_legal (persona_ci, empresa_id, fecha_eleccion, numero_poder)
+VALUES (123456, 1, '2022-01-01', 123),
+       (234567, 2, '2022-02-01', 234),
+       (345678, 3, '2022-03-01', 345),
+       (456789, 4, '2022-04-01', 456),
+       (567890, 5, '2022-05-01', 567),
+       (678901, 6, '2022-06-01', 678),
+       (789012, 7, '2022-07-01', 789),
+       (890123, 8, '2022-08-01', 890),
+       (901234, 9, '2022-09-01', 901),
+       (123456, 10, '2022-10-01', 1234);
+
+-- Tabla: duenho
+INSERT INTO duenho (persona_ci, empresa_id)
+VALUES (123456, 1),
+       (234567, 2),
+       (345678, 3),
+       (456789, 4),
+       (567890, 5),
+       (678901, 6),
+       (789012, 7),
+       (890123, 8),
+       (901234, 9),
+       (123456, 10);
+
+-- Tabla: empleado
+INSERT INTO empleado (persona_ci, empresa_id, cargo, salario, fecha_inicio, fecha_fin)
+VALUES (123456, 1, 'Gerente de Ventas', 5000.00, '2021-01-01', NULL),
+       (234567, 2, 'Analista de Marketing', 3000.00, '2022-02-01', NULL),
+       (345678, 3, 'Desarrollador de Software', 4000.00, '2023-03-01', NULL),
+       (456789, 4, 'Contador', 3500.00, '2022-04-01', '2023-04-01'),
+       (567890, 5, 'Diseñador Gráfico', 3200.00, '2023-05-01', NULL),
+       (678901, 6, 'Especialista en RRHH', 3800.00, '2021-06-01', NULL),
+       (789012, 7, 'Ingeniero de Proyectos', 4500.00, '2022-07-01', NULL),
+       (890123, 8, 'Analista Financiero', 4000.00, '2023-08-01', NULL),
+       (901234, 9, 'Consultor de Ventas', 5500.00, '2021-09-01', NULL),
+       (123456, 10, 'Ejecutivo de Cuentas', 4800.00, '2022-10-01', NULL);
+
+-- Tabla: sucursal
+INSERT INTO sucursal (empresa_id, direccion_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 4),
+       (5, 5),
+       (6, 6),
+       (7, 7),
+       (8, 8),
+       (9, 9),
+       (10, 10);
+
+
